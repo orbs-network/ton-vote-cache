1861c1869a5921745446493f56df32db7b71b6be
Ami Hazbany <ami@orbs.com>
2023-11-10 11:38:17 +0200

    decrease batch size

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -15,11 +15,11 @@ import { getConfigProposalResults } from "./validators/validators-config";
 
 dotenv.config();
 
-const DAOS_BATCH_SIZE = 100;
-const PROPOSALS_BATCH_SIZE = 100;
+const DAOS_BATCH_SIZE = 50;
+const PROPOSALS_BATCH_SIZE = 50;
 
-const UPDATE_DAOS_BATCH_SIZE = 100;
-const PROPOSAL_METADATA_BATCH_SIZE = 100;
+const UPDATE_DAOS_BATCH_SIZE = 50;
+const PROPOSAL_METADATA_BATCH_SIZE = 50;
 
 
 const RELEASE_MODE = Number(process.env.RELEASE_MODE) as ReleaseMode
